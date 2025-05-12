01f477556acbed41e44d4468bcfdea660431c0ec
shamodhas <shamodha7@gmail.com>
2025-05-12 06:51:14 +0530

    initial commit

---
+++ src/main/java/lk/ijse/test/SampleController.java
@@ -0,0 +1,26 @@
+package lk.ijse.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * --------------------------------------------
+ * Author: Shamodha Sahan
+ * GitHub: https://github.com/shamodhas
+ * Website: https://shamodha.com
+ * --------------------------------------------
+ * Created: 5/12/2025 6:18 AM
+ * Project: Test
+ * --------------------------------------------
+ **/
+
+@RestController
+@RequestMapping("/api")
+public class SampleController {
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from Spring Boot!";
+    }
+}
+
